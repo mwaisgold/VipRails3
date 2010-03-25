7ff9220ac24a9bcb48a19c693c61b0f69f203836
guido <guidogelman@gmail.com>
2010-03-25 11:05:15 -0400

    Wrap the seed method inside a transaction.

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,35 +18,40 @@ Question.delete_all
 Calification.delete_all
 Site.delete_all
 
+total = (ENV["ITEMS"] || 1).to_i
 
-@site = Site.create site_id: "MLA", locale: "es"
-@shipMethod = ShipMethod.create description: "A convenir"
-@paymentMethod = PaymentMethod.create name: "visa", logo: "sarasa"
-@product = CatalogProduct.create name: "Iphone mejor del mundo"
-@attr = CatalogProductAttribute.create key: "Wifi", value: "(802.11b/g)"
-@product.catalog_product_attributes << @attr
-@product.save
+ActiveRecord::Base.transaction do
+  @site = Site.create site_id: "MLA", locale: "es"
+  @shipMethod = ShipMethod.create description: "A convenir"
+  @paymentMethod = PaymentMethod.create name: "visa", logo: "sarasa"
+  @product = CatalogProduct.create name: "Iphone mejor del mundo"
+  @attr = CatalogProductAttribute.create key: "Wifi", value: "(802.11b/g)"
+  @product.catalog_product_attributes << @attr
+  @product.save
 
-1.times do |i|
-  @customer = Customer.create nickname: "customer#{i}", points: 95, qty_calif: 100
-  @customer2 = Customer.create nickname: "other#{i}", points: 45, qty_calif: 50
+  total.times do |i|
+    @customer = Customer.create nickname: "customer#{i}", points: 95, qty_calif: 100
+    @customer2 = Customer.create nickname: "other#{i}", points: 45, qty_calif: 50
 
-  @item = Item.create title: "iPod touch 32gb 3ra generacion, caja sellada", price: 100, description: "description", image: "61826546_3253.jpg", bids_count: 35, site: @site, customer: @customer
+    @item = Item.create title: "iPod touch 32gb 3ra generacion, caja sellada", price: 100, description: "description", image: "61826546_3253.jpg", bids_count: 35, site: @site, customer: @customer
 
-  @review = Review.create title: "Titulo de review", pros: "prossss", contras: "contrass", customer: @customer, catalog_product: @product, qty_votes: 10, qty_pos: 5, points: 4, conclusion: "conclusion"
+    @review = Review.create title: "Titulo de review", pros: "prossss", contras: "contrass", customer: @customer, catalog_product: @product, qty_votes: 10, qty_pos: 5, points: 4, conclusion: "conclusion"
 
-  30.times do |j|
-    @question = Question.create item: @item, question: "preguntita#{j}", question_dt: Time.now, answer: "respuestita#{j}", answer_dt: Time.now
-  end
+    30.times do |j|
+      @question = Question.create item: @item, question: "preguntita#{j}", question_dt: Time.now, answer: "respuestita#{j}", answer_dt: Time.now
+    end
 
-  @calification = Calification.create customer: @customer2, item: @item, texto_calif: "todo barbaro", value_calif: 1, fecha: Time.now
+    @calification = Calification.create customer: @customer2, item: @item, texto_calif: "todo barbaro", value_calif: 1, fecha: Time.now
 
-  @item.catalog_product = @product
-  @item.payment_methods << @paymentMethod
-  @item.ship_methods << @shipMethod
-  @item.save
+    @item.catalog_product = @product
+    @item.payment_methods << @paymentMethod
+    @item.ship_methods << @shipMethod
+    @item.save
 
-  5.times do
-    Item.create title: "Filmadora Jvc Everio Gz-mg630 Disco Rigido 60gb Modelo 2009", price: 10000, description: "description", image: "image.jpg", bids_count: 35, site: @site, customer: @customer
+    puts @item.id
+
+    # 5.times do
+      # Item.create title: "Filmadora Jvc Everio Gz-mg630 Disco Rigido 60gb Modelo 2009", price: 10000, description: "description", image: "image.jpg", bids_count: 35, site: @site, customer: @customer
+    # end
   end
 end
