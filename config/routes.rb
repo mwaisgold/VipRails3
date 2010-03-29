5013fe0db2e7faa7e7c942106002f2b39647880c
mwaisgold <matias.waisgold@mercadolibre.com>
2010-03-29 12:50:41 -0400

    Agregado de los locales

--- config/routes.rb
+++ config/routes.rb
@@ -70,4 +70,8 @@ VipRails3::Application.routes.draw do |map|
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
    match ':controller(/:action(/:id(.:format)))'
+
+   Translate::Routes.translation_ui(map) if RAILS_ENV != "production"
 end
+
+
